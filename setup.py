453f07792ecf83241e78b0d4eeb70bbd6aa9d004
B Nikhil Sharma <b.nikhil.sharma@gmail.com>
2025-03-28 13:39:31 +0530

    created structure of the project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="B Nikhil Sharma",
+    author_email="b.nikhil.sharma@gmail.com",
+    packages=find_packages()
+)
